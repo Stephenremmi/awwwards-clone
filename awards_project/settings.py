f90ff856e7c479cc3ffa33cf51515991b5f5aaf2
stephen remmi <stephenremmi@gmail.com>
2020-07-15 14:16:43 +0300

    Configure whitenoise in base settings

--- awards_project/settings.py
+++ awards_project/settings.py
@@ -143,6 +143,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
